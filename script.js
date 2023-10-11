2ad02e63329a4db63e1b2d7c0cebeba56c51c107
patriciosebastian <psebastiansalazar@gmail.com>
2023-10-10 18:33:14 -0700

    large commit of general updates
    -still have placeholder information
    -work in progress

---
+++ script.js
@@ -0,0 +1,39 @@
+// Form Submit
+const form = document.querySelector(".contact-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  const status = document.querySelector(".form-status");
+  const data = new FormData(event.target);
+
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: { 'Accept': "application/json" },
+  })
+    .then((response) => {
+      if (response.ok) {
+        status.classList.add('message-success');
+        status.innerHTML = "Success! I'll be in touch as soon as possible.";
+        form.reset();
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, "errors")) {
+            status.classList.add('message-error');
+            status.innerHTML = data["errors"]
+              .map((error) => error["message"])
+              .join(", ");
+          } else {
+            status.classList.add('message-error');
+            status.innerHTML = "Oops! There was a problem sending your message. Please try again.";
+          }
+        });
+      }
+    })
+    .catch((error) => {
+      status.classList.add('message-error');
+      status.innerHTML = "Oops! There was a problem sending your message. Please try again.";
+      console.log("Error:", error);
+    });
+}
+form.addEventListener("submit", handleSubmit);
