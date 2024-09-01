d49ae430002b609f9a366ade373b394e7d65c42f
Patricio Salazar <psalazar@empire-medical.com>
2024-08-31 21:51:44 -0700

    set up theme provider

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
-import { Inter } from "next/font/google";
-import "../styles/globals.css";
+import { Inter } from "next/font/google"
+import { ThemeProvider } from "@/components/theme-provider"
+import "../styles/globals.css"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +11,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className + ` container h-full mx-auto`}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className + ` container h-full mx-auto`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
