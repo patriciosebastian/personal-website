28b7db8c2ccc8ac80e2a03e111d7dc79a77d5ae1
Patricio Salazar <psalazar@empire-medical.com>
2024-09-12 06:51:38 -0700

    made small text in footer muted color

--- src/components/main-footer.jsx
+++ src/components/main-footer.jsx
@@ -8,8 +8,10 @@ export default function MainFooter() {
         <Link href="/freelance">Freelance</Link>
         <Link href="/blog">Blog</Link>
       </div>
-      <small className="block text-center mb-2">Your Web Dev &amp; Friendly Internet Neighbor</small>
-      <div className="flex justify-center items-center bg-primary h-[1px] content-none lg:mb-4"><small className="block text-center bg-background px-1">&copy; 2024</small></div>
+      <small className="block text-center mb-2 text-muted-foreground">Your Web Dev &amp; Internet Neighbor</small>
+      <div className="flex justify-center items-center bg-muted-foreground h-[1px] content-none lg:mb-4 text-muted-foreground">
+        <small className="block text-center bg-background px-1">&copy; 2024</small>
+      </div>
       <div className="text-center text-[2.3rem] text-nowrap tracking-[-2px]">PATRICIO SALAZAR</div>
     </div>
   );
