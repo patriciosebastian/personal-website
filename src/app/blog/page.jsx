056743432453be6b77f947000f031d113e887c47
Patricio Salazar <psalazar@empire-medical.com>
2024-09-13 14:57:07 -0700

    added responsive blog page styles

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -20,33 +20,35 @@ export default async function BlogPage() {
   return (
     <>
       <MobileNav />
-      <div className="h-56 flex flex-col justify-center items-start gap-4 border-b-[1px] mb-8">
-        <h1 className="text-4xl mt-14">Blog</h1>
-        <p className="text-pretty">Look at you! Reading my blog &#129395;<br />
-        I write about web development, entrepreneurialism, and things in life I care about.</p>
+      <div className="max-w-lg lg:max-w-3xl mx-auto">
+        <div className="h-56 flex flex-col justify-center items-start gap-4 border-b-[1px] mb-8">
+          <h1 className="text-4xl mt-14">Blog</h1>
+          <p className="text-pretty">Look at you! Reading my blog &#129395;<br />
+          I write about web development, entrepreneurialism, and things in life I care about.</p>
+        </div>
+        <ul>
+          {blogs.map((blog) => (
+            <li key={blog.id}>
+              <Card className="mb-4">
+                <CardHeader>
+                  <CardTitle className="text-balance"><Link href={`/blog/${blog.slug}`}>{blog.title}</Link></CardTitle>
+                  <CardDescription className="text-primary">
+                    {blog.sub_title}
+                  </CardDescription>
+                </CardHeader>
+                <CardFooter className="text-gray-500">
+                  {(blog.is_freelance) ? <Badge variant="outline" className="mr-2">freelance</Badge> : null}
+                  {(blog.is_web_development) ? <Badge variant="outline" className="mr-2">web development</Badge> : null}
+                  {(blog.is_tech) ? <Badge variant="outline" className="mr-2">tech</Badge> : null}
+                  {(blog.is_entrepreneurialism) ? <Badge variant="outline" className="mr-2">entrepreneurialism</Badge> : null}
+                  {(blog.is_life) ? <Badge variant="outline" className="mr-2">life</Badge> : null}
+                  <small className="ml-auto">{new Date(blog.created_at).toLocaleDateString()}</small>
+                </CardFooter>
+              </Card>
+            </li>
+          ))}
+        </ul>
       </div>
-      <ul>
-        {blogs.map((blog) => (
-          <li key={blog.id}>
-            <Card className="mb-4">
-              <CardHeader>
-                <CardTitle className="text-balance"><Link href={`/blog/${blog.slug}`}>{blog.title}</Link></CardTitle>
-                <CardDescription className="text-primary">
-                  {blog.sub_title}
-                </CardDescription>
-              </CardHeader>
-              <CardFooter className="text-gray-500">
-                {(blog.is_freelance) ? <Badge variant="outline" className="mr-2">freelance</Badge> : null}
-                {(blog.is_web_development) ? <Badge variant="outline" className="mr-2">web development</Badge> : null}
-                {(blog.is_tech) ? <Badge variant="outline" className="mr-2">tech</Badge> : null}
-                {(blog.is_entrepreneurialism) ? <Badge variant="outline" className="mr-2">entrepreneurialism</Badge> : null}
-                {(blog.is_life) ? <Badge variant="outline" className="mr-2">life</Badge> : null}
-                <small className="ml-auto">{new Date(blog.created_at).toLocaleDateString()}</small>
-              </CardFooter>
-            </Card>
-          </li>
-        ))}
-      </ul>
 
       <Spacer />
 
