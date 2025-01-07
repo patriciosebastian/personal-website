1b7572b098df9427062170b317563e1ebcfea881
Patricio Salazar <psalazar@empire-medical.com>
2025-01-07 02:54:28 -0800

    added the remaining post properties

--- src/components/tinyMCE.jsx
+++ src/components/tinyMCE.jsx
@@ -3,12 +3,47 @@ import { Editor } from '@tinymce/tinymce-react'
 import { createClient } from '../utils/supabase/client'
 import { Button } from './ui/button'
 
-export default function TinyMCE() {
+export default function TinyMCE({
+  title,
+  subTitle,
+  preview,
+  isFreelance,
+  isWebDevelopment,
+  slug,
+  isTech,
+  isLife,
+  isEntrepreneurship,
+  isSideProject,
+  isProductReview,
+  isThoughts
+}) {
   const editorRef = useRef(null);
   const supabase = createClient();
 
   const handlePublish = async () => {
     // 
+  };
+
+  // Keep this log for personal use
+  const logContent = async () => {
+    if (editorRef.current) {
+      const content = editorRef.current.getContent();
+      console.log('Title:', title);
+      console.log(content);
+      console.log('Other post properties:', {
+        'sub title': subTitle,
+        'preview': preview,
+        'is freelance': isFreelance,
+        'is web development': isWebDevelopment,
+        'slug': slug,
+        'is tech': isTech,
+        'is life': isLife,
+        'is entrepreneurship': isEntrepreneurship,
+        'is side project': isSideProject,
+        'is product review': isProductReview,
+        'is thoughts': isThoughts,
+      });
+    }
   };
 
   return (
@@ -19,21 +54,23 @@ export default function TinyMCE() {
         init={{
           height: 600,
           menubar: true,
-          plugins: [
-            'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
-          ],
-          toolbar: 'undo redo | blocks fontfamily fontsize | \
-                    bold italic underline strikethrough | \
-                    link image media table | \
-                    align lineheight | numlist bullist indent outdent | \
-                    emoticons charmap | removeformat'
+          plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
+          toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
         }}
       />
       <Button
         onClick={handlePublish}
-        className="mt-4"
+        className="mt-4 mr-2"
+        disabled
       >
         Publish
+      </Button>
+      <Button
+        onClick={logContent}
+        className="mt-4 text-secondary"
+        variant="ghost"
+      >
+        Log Content
       </Button>
     </>
   );
