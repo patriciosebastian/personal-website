4f695b018ea8b94982371600e32f6dc0f2cfbdd5
Patricio Salazar <psalazar@empire-medical.com>
2025-01-12 16:44:30 -0800

    updated tinymce images_upload_handler to Promise based syntax

--- src/components/tinyMCE.jsx
+++ src/components/tinyMCE.jsx
@@ -85,36 +85,32 @@ export default function TinyMCE({
           toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
           paste_data_images: true,
           image_advtab: true,
-          images_upload_handler: async (blobInfo, success, failure) => {
-            try {
-              const file = blobInfo.blob();
-              const fileName = blobInfo.filename();
-              const filePath = `tinymce-images/${Date.now()}-${fileName}`;
+          images_upload_handler: async (blobInfo) => {
+            const file = blobInfo.blob();
+            const fileName = blobInfo.filename();
+            const filePath = `tinymce-images/${Date.now()}-${fileName}`;
+            return new Promise(async (resolve, reject) => {
+              try {
+                const { data: uploadData, error } = await supabase
+                  .storage
+                  .from('personal-website')
+                  .upload(filePath, file, {
+                    contentType: file.type,
+                  });
 
-              const { data: uploadData, error: uploadError } = await supabase
-                .storage
-                .from('personal-website')
-                .upload(filePath, file, {
-                  contentType: file.type,
-                });
+                if (error) {
+                  console.error('Error uploading image:', error);
+                }
 
-              if (uploadError) {
-                console.error('Error uploading image:', uploadError.message);
-                failure('Error uploading image');
-                throw uploadError;
+                const publicUrl = process.env.NEXT_PUBLIC_SUPABASE_URL + '/storage/v1/object/public/personal-website/' + filePath;
+
+                console.log('Image uploaded successfully:', publicUrl);
+                resolve(publicUrl);
+              } catch (error) {
+                console.error('Error uploading image:', error);
+                reject('Error uploading image:', error);
               }
-
-              const { data: { publicUrl } } = supabase.storage.from('personal-website').getPublicUrl(filePath);
-
-              if (!publicUrl) {
-                console.log('No "publicURL" found for image:', filePath);
-              }
-
-              console.log('Image uploaded successfully:', publicUrl);
-              success(publicUrl);
-            } catch (error) {
-              failure(error.message);
-            }
+            });
           },
         }}
       />
