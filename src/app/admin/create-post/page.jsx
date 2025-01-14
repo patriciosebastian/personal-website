7ad3326665a97e2b9ea5e1cbdf6a94a0602fe509
Patricio Salazar <psalazar@empire-medical.com>
2025-01-14 07:58:38 -0800

    added meta robots tag in Head to two pages

--- src/app/admin/create-post/page.jsx
+++ src/app/admin/create-post/page.jsx
@@ -1,10 +1,11 @@
 'use client'
 
-import TinyMCE from "@/components/tinyMCE"
 import { Button } from "@/components/ui/button"
-import Expandable from "@/components/ui/expandable"
 import { createClient } from "@/utils/supabase/client"
 import { useEffect, useState } from "react"
+import TinyMCE from "@/components/tinyMCE"
+import Expandable from "@/components/ui/expandable"
+import Head from "next/head"
 
 export default function CreatePostPage() {
   const [user, setUser] = useState(null);
@@ -59,73 +60,78 @@ export default function CreatePostPage() {
   };
 
   return (
-    <div className="relative min-h-svh place-content-center">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight text-center lg:text-5xl">Create New Post</h1>
-      <Button className="absolute top-2 right-0 flex items-center space-x-2 text-sm text-secondary" onClick={handleLogout} variant="ghost">Logout</Button>
-      <div className="mb-2">
-        <label htmlFor="title" className="text-muted-foreground">Title:</label>
-        <input
-          type="text"
-          name="title"
-          id="title"
-          className="block p-2 rounded w-full bg-background border-2 border-background hover:border-secondary"
-          required
-          onChange={(e) => setTitle(e.target.value)}
+    <>
+      <Head>
+        <meta name="robots" content="noindex, nofollow" />
+      </Head>
+      <div className="relative min-h-svh place-content-center">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight text-center lg:text-5xl">Create New Post</h1>
+        <Button className="absolute top-2 right-0 flex items-center space-x-2 text-sm text-secondary" onClick={handleLogout} variant="ghost">Logout</Button>
+        <div className="mb-2">
+          <label htmlFor="title" className="text-muted-foreground">Title:</label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            className="block p-2 rounded w-full bg-background border-2 border-background hover:border-secondary"
+            required
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <Expandable trigger="Post details" className="text-base text-muted-foreground mb-2">
+          <div className="grid grid-cols-3 items-center gap-4 text-black dark:text-white p-4 bg-secondary rounded-sm">
+            <input type="text" name="subTitle" id="subTitle" placeholder="sub_title" className="p-2" onChange={(e) => setSubTitle(e.target.value)} />
+            <input type="text" name="preview" id="preview" placeholder="preview" className="p-2" onChange={(e) => setPreview(e.target.value)} />
+            <input type="text" name="slug" id="slug" placeholder="slug" className="p-2" onChange={(e) => setSlug(e.target.value)} />
+            <div>
+              <input type="checkbox" name="isFreelance" id="isFreelance" className="mr-2 align-middle" onChange={(e) => setIsFreelance(e.target.checked)} />
+              <label htmlFor="isFreelance" className="align-middle">is_freelance</label>
+            </div>
+            <div>
+              <input type="checkbox" name="isWebDevelopment" id="isWebDevelopment" className="mr-2 align-middle" onChange={(e) => setIsWebDevelopment(e.target.checked)} />
+              <label htmlFor="isWebDevelopment" className="align-middle">is_web_development</label>
+            </div>
+            <div>
+              <input type="checkbox" name="isTech" id="isTech" className="mr-2 align-middle" onChange={(e) => setIsTech(e.target.checked)} />
+              <label htmlFor="isTech" className="align-middle">is_tech</label>
+            </div>
+            <div>
+              <input type="checkbox" name="isLife" id="isLife" className="mr-2 align-middle" onChange={(e) => setIsLife(e.target.checked)} />
+              <label htmlFor="isLife" className="align-middle">is_life</label>
+            </div>
+            <div>
+              <input type="checkbox" name="isEntrepreneurship" id="isEntrepreneurship" className="mr-2 align-middle" onChange={(e) => setIsEntrepreneurship(e.target.checked)} />
+              <label htmlFor="isEntrepreneurship" className="align-middle">is_entrepreneurship</label>
+            </div>
+            <div>
+              <input type="checkbox" name="isSideProject" id="isSideProject" className="mr-2 align-middle" onChange={(e) => setIsSideProject(e.target.checked)} />
+              <label htmlFor="isSideProject" className="align-middle">is_side_project</label>
+            </div>
+            <div>
+              <input type="checkbox" name="isProductReview" id="isProductReview" className="mr-2 align-middle" onChange={(e) => setIsProductReview(e.target.checked)} />
+              <label htmlFor="isProductReview" className="align-middle">is_product_review</label>
+            </div>
+            <div>
+              <input type="checkbox" name="isThoughts" id="isThoughts" className="mr-2 align-middle" onChange={(e) => setIsThoughts(e.target.checked)} />
+              <label htmlFor="isThoughts" className="align-middle">is_thoughts</label>
+            </div>
+          </div>
+        </Expandable>
+        <TinyMCE
+          title={title}
+          subTitle={subTitle}
+          preview={preview}
+          slug={slug}
+          isFreelance={isFreelance}
+          isWebDevelopment={isWebDevelopment}
+          isTech={isTech}
+          isLife={isLife}
+          isEntrepreneurship={isEntrepreneurship}
+          isSideProject={isSideProject}
+          isProductReview={isProductReview}
+          isThoughts={isThoughts}
         />
       </div>
-      <Expandable trigger="Post details" className="text-base text-muted-foreground mb-2">
-        <div className="grid grid-cols-3 items-center gap-4 text-black dark:text-white p-4 bg-secondary rounded-sm">
-          <input type="text" name="subTitle" id="subTitle" placeholder="sub_title" className="p-2" onChange={(e) => setSubTitle(e.target.value)} />
-          <input type="text" name="preview" id="preview" placeholder="preview" className="p-2" onChange={(e) => setPreview(e.target.value)} />
-          <input type="text" name="slug" id="slug" placeholder="slug" className="p-2" onChange={(e) => setSlug(e.target.value)} />
-          <div>
-            <input type="checkbox" name="isFreelance" id="isFreelance" className="mr-2 align-middle" onChange={(e) => setIsFreelance(e.target.checked)} />
-            <label htmlFor="isFreelance" className="align-middle">is_freelance</label>
-          </div>
-          <div>
-            <input type="checkbox" name="isWebDevelopment" id="isWebDevelopment" className="mr-2 align-middle" onChange={(e) => setIsWebDevelopment(e.target.checked)} />
-            <label htmlFor="isWebDevelopment" className="align-middle">is_web_development</label>
-          </div>
-          <div>
-            <input type="checkbox" name="isTech" id="isTech" className="mr-2 align-middle" onChange={(e) => setIsTech(e.target.checked)} />
-            <label htmlFor="isTech" className="align-middle">is_tech</label>
-          </div>
-          <div>
-            <input type="checkbox" name="isLife" id="isLife" className="mr-2 align-middle" onChange={(e) => setIsLife(e.target.checked)} />
-            <label htmlFor="isLife" className="align-middle">is_life</label>
-          </div>
-          <div>
-            <input type="checkbox" name="isEntrepreneurship" id="isEntrepreneurship" className="mr-2 align-middle" onChange={(e) => setIsEntrepreneurship(e.target.checked)} />
-            <label htmlFor="isEntrepreneurship" className="align-middle">is_entrepreneurship</label>
-          </div>
-          <div>
-            <input type="checkbox" name="isSideProject" id="isSideProject" className="mr-2 align-middle" onChange={(e) => setIsSideProject(e.target.checked)} />
-            <label htmlFor="isSideProject" className="align-middle">is_side_project</label>
-          </div>
-          <div>
-            <input type="checkbox" name="isProductReview" id="isProductReview" className="mr-2 align-middle" onChange={(e) => setIsProductReview(e.target.checked)} />
-            <label htmlFor="isProductReview" className="align-middle">is_product_review</label>
-          </div>
-          <div>
-            <input type="checkbox" name="isThoughts" id="isThoughts" className="mr-2 align-middle" onChange={(e) => setIsThoughts(e.target.checked)} />
-            <label htmlFor="isThoughts" className="align-middle">is_thoughts</label>
-          </div>
-        </div>
-      </Expandable>
-      <TinyMCE
-        title={title}
-        subTitle={subTitle}
-        preview={preview}
-        slug={slug}
-        isFreelance={isFreelance}
-        isWebDevelopment={isWebDevelopment}
-        isTech={isTech}
-        isLife={isLife}
-        isEntrepreneurship={isEntrepreneurship}
-        isSideProject={isSideProject}
-        isProductReview={isProductReview}
-        isThoughts={isThoughts}
-      />
-    </div>
+    </>
   );
 }
--- src/app/secret-login/page.jsx
+++ src/app/secret-login/page.jsx
@@ -6,6 +6,7 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import { Loader2 } from "lucide-react"
 import Image from 'next/image'
+import Head from 'next/head'
 
 export default function SecretLoginPage() {
   const [email, setEmail] = useState('');
@@ -32,52 +33,57 @@ export default function SecretLoginPage() {
   };
 
   return (
-    <div className="min-h-screen place-content-center flex flex-col gap-6">
-      <Card className='h-1/2 overflow-hidden'>
-        <CardContent className='h-full grid p-0 md:grid-cols-2'>
-          <div className='relative hidden bg-muted md:block'>
-            <Image
-              src='https://nsdysbxlabtmtxscxqvw.supabase.co/storage/v1/object/public/personal-website/Misc/bixby-creek-bridge-big-sur.jpg'
-              alt='Bixy Creek Bridge and Big Sur'
-              className='absolute inset-0 h-full w-full object-cover object-center'
-              width={2191}
-              height={1460}
-            />
-          </div>
-          <form onSubmit={handleLogin} className='p-6 md:p-8 place-content-center relative'>
-            <div className='flex flex-col'>
-              <div className='flex flex-col items-center text-center mb-6'>
-                <h1 className='text-2xl font-bold'>Log in</h1>
-                <p className='text-sm text-balance text-muted-foreground'>
-                  Welcome back, Patricio.
-                </p>
+    <>
+      <Head>
+        <meta name="robots" content="noindex, nofollow" />
+      </Head>
+      <div className="min-h-screen place-content-center flex flex-col gap-6">
+        <Card className='h-1/2 overflow-hidden'>
+          <CardContent className='h-full grid p-0 md:grid-cols-2'>
+            <div className='relative hidden bg-muted md:block'>
+              <Image
+                src='https://nsdysbxlabtmtxscxqvw.supabase.co/storage/v1/object/public/personal-website/Misc/bixby-creek-bridge-big-sur.jpg'
+                alt='Bixy Creek Bridge and Big Sur'
+                className='absolute inset-0 h-full w-full object-cover object-center'
+                width={2191}
+                height={1460}
+              />
+            </div>
+            <form onSubmit={handleLogin} className='p-6 md:p-8 place-content-center relative'>
+              <div className='flex flex-col'>
+                <div className='flex flex-col items-center text-center mb-6'>
+                  <h1 className='text-2xl font-bold'>Log in</h1>
+                  <p className='text-sm text-balance text-muted-foreground'>
+                    Welcome back, Patricio.
+                  </p>
+                </div>
+                <input
+                  type='email'
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  className='p-2 mb-2 rounded'
+                />
+                <input
+                  type='password'
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  className='p-2 mb-2 rounded'
+                />
+                <Button type="submit" disabled={loading} className='mt-2'>
+                  {loading ? <Loader2 className="animate-spin" /> : 'Log in'}
+                </Button>
               </div>
-              <input
-                type='email'
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-                className='p-2 mb-2 rounded'
-              />
-              <input
-                type='password'
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-                className='p-2 mb-2 rounded'
-              />
-              <Button type="submit" disabled={loading} className='mt-2'>
-                {loading ? <Loader2 className="animate-spin" /> : 'Log in'}
-              </Button>
-            </div>
-            <div className='text-center text-muted-foreground text-sm absolute bottom-0 left-0 right-0 mb-[.325rem]'>
-              Authorized personnel only.
-            </div>
-          </form>
-        </CardContent>
-      </Card>
-    </div>
+              <div className='text-center text-muted-foreground text-sm absolute bottom-0 left-0 right-0 mb-[.325rem]'>
+                Authorized personnel only.
+              </div>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </>
   );
 }
