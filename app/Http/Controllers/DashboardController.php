bc51d7f6c549e752f4760b74238c4c0f17b0d7da
Patricio Salazar <psebastiansalazar@gmail.com>
2025-11-29 23:26:02 -0800

    built props for posts, drafts, and reactions to pass to dashboard page

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\Reaction;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -9,6 +11,25 @@ class DashboardController extends Controller
 {
     public function index(Request $request)
     {
-        return Inertia::render('dashboard/dashboard');
+        $publishedPosts = Post::where('status', 'published')
+            ->latest()
+            ->paginate(10);
+
+        $draftPosts = Post::where('status', 'draft')
+            ->latest()
+            ->paginate(10);
+
+        $reactions = Reaction::query()->with('post', function ($query) {
+            $query->select('id', 'title');
+        })
+            ->latest()
+            ->take(10)
+            ->get();
+
+        return Inertia::render('dashboard/dashboard', [
+            'publishedPosts' => $publishedPosts,
+            'draftPosts' => $draftPosts,
+            'reactions' => $reactions,
+        ]);
     }
 }
