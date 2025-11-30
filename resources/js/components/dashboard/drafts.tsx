a2e8d969186321c449e55b9935ff4f85814923e3
Patricio Salazar <psebastiansalazar@gmail.com>
2025-11-29 23:18:32 -0800

    created drafts and reactions dashboard component files

---
+++ resources/js/components/dashboard/drafts.tsx
@@ -0,0 +1,5 @@
+export default function Drafts(params) {
+    return (
+        <div>Drafts</div>
+    );
+};
---
+++ resources/js/components/dashboard/reactions.tsx
@@ -0,0 +1,45 @@
+import { Reaction } from "@/types"
+import { Item, ItemContent, ItemMedia, ItemTitle } from "../ui/item"
+import { useState } from "react"
+
+interface ReactionsProps {
+    reactions: Reaction[];
+}
+
+export default function Reactions({ reactions }: ReactionsProps) {
+    const [date, setDate] = useState<string>("");
+    const [time, setTime] = useState<string>("");
+
+    function formatDateAndTime(created_at: string) {
+        setDate(created_at.split(',')[0]);
+        setTime(created_at.split(',')[1]);
+
+        return { date, time };
+    }
+
+    return (
+        <>
+            <div className="text-lg pl-4 mb-4 font-semibold">Reactions</div>
+            <ul>
+                {reactions.map(reaction => (
+                    <Item
+                        variant={"outline"}
+                        className="items-start"
+                        key={reaction.id}
+                    >
+                        <ItemMedia>
+                            {/* emoji here */}
+                        </ItemMedia>
+                        <ItemContent>
+                            <ItemTitle className="text-xs">{reaction.post.title}</ItemTitle>
+                        </ItemContent>
+                        <ItemContent>
+                            <p className="text-xs">{formatDateAndTime(new Date(reaction.created_at).toLocaleString()).date}</p>
+                            <p className="text-xs">{formatDateAndTime(new Date(reaction.created_at).toLocaleString()).time}</p>
+                        </ItemContent>
+                    </Item>
+                ))}
+            </ul>
+        </>
+    );
+};
