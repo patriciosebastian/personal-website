9037e5bd56cc3310e016e3feed58157753ee982e
Patricio Salazar <psebastiansalazar@gmail.com>
2025-12-01 14:26:42 -0800

    use new components on dashboard page

--- resources/js/pages/dashboard/dashboard.tsx
+++ resources/js/pages/dashboard/dashboard.tsx
@@ -1,9 +1,11 @@
-import { PlaceholderPattern } from '@/components/ui/placeholder-pattern'
 import AppLayout from '@/layouts/app-layout'
 import { index as dashboard } from '@/actions/App/Http/Controllers/DashboardController'
-import { type BreadcrumbItem } from '@/types'
+import { PaginatedData, Post, Reaction, type BreadcrumbItem } from '@/types'
 import { Head } from '@inertiajs/react'
 import PublishedPosts from '@/components/dashboard/published-posts'
+import Drafts from '@/components/dashboard/drafts'
+import Reactions from '@/components/dashboard/reactions'
+import ViewSelectedPost from '@/components/dashboard/view-selected-post'
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -12,25 +14,32 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
-export default function Dashboard() {
+interface DashboardProps {
+    publishedPosts: PaginatedData<Post>;
+    draftPosts: Post[];
+    reactions: Reaction[];
+    postToPreview: Post[];
+}
+
+export default function Dashboard({ publishedPosts, draftPosts, reactions, postToPreview }: DashboardProps) {
+    console.log({ publishedPosts, draftPosts, reactions, postToPreview });
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PublishedPosts />
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    <div className="relative aspect-video overflow-x-hidden scrollbar-thin rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                        <PublishedPosts publishedPosts={publishedPosts} />
                     </div>
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    <div className="relative aspect-video overflow-hidden scrollbar-thin rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                        <Drafts />
                     </div>
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    <div className="relative aspect-video overflow-x-hidden scrollbar-thin rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                        <Reactions reactions={reactions} />
                     </div>
                 </div>
-                <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
-                    <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                <div className="relative flex-1 overflow-hidden scrollbar-thin rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
+                    <ViewSelectedPost postToPreview={postToPreview || publishedPosts.data[0]} />
                 </div>
             </div>
         </AppLayout>
