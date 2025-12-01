c209c25fd8a2535df326dddbca41b50a127d99f3
Patricio Salazar <psebastiansalazar@gmail.com>
2025-12-01 11:21:30 -0800

    built out published-posts component and refactored formatDateAndTime into a hook

--- resources/js/components/dashboard/published-posts.tsx
+++ resources/js/components/dashboard/published-posts.tsx
@@ -1,5 +1,56 @@
-export default function PublishedPosts(params) {
+import { PaginatedData, Post } from "@/types"
+import { Item, ItemContent, ItemDescription, ItemTitle } from "../ui/item"
+import { useFormatDateAndTime } from "@/hooks/use-format-date-time"
+import { Link } from "@inertiajs/react"
+import { useRoute } from "ziggy-js"
+import { useEffect, useState } from "react"
+
+interface PublishedPostsProps {
+    publishedPosts: PaginatedData<Post>;
+}
+
+export default function PublishedPosts({ publishedPosts }: PublishedPostsProps) {
+    const formatDateAndTime = useFormatDateAndTime;
+    const route = useRoute();
+    const selectedPostSlug = route().params.post as string | undefined;
+    const [selectedPost, setSelectedPost] = useState("");
+
+    useEffect(() => {
+        setSelectedPost(selectedPostSlug || "");
+    }, [selectedPostSlug]);
+
     return (
-        <div>All Published Posts</div>
-    );    
+        <div>
+            <div className="text-lg pl-6 my-4 font-semibold">Published Posts</div>
+            <ul className="space-y-4 px-6 mb-6">
+                {publishedPosts.data.map(post => {
+                    const { date, time } = formatDateAndTime(post.published_at ? new Date(post.published_at).toLocaleString() : "");
+
+                    return (
+                        <Item
+                            variant={"muted"}
+                            className={`items-start ${selectedPost === post.slug
+                                ? 'bg-accent dark:bg-accent'
+                                : 'bg-muted/10 dark:bg-muted/20 hover:bg-muted/60 dark:hover:bg-muted/30'}`}
+                            key={post.id}
+                        >
+                            <Link
+                                href={route('dashboard.index', { post: post.slug })}
+                                className="flex items-start"
+                            >
+                                <ItemContent>
+                                    <ItemTitle>{post.title}</ItemTitle>
+                                    <ItemDescription>{post.preview_text}</ItemDescription>
+                                </ItemContent>
+                                <ItemContent>
+                                    <p>{date}</p>
+                                    <p className="text-xs text-muted-foreground">{time}</p>
+                                </ItemContent>
+                            </Link>
+                        </Item>
+                    );
+                })}
+            </ul>
+        </div>
+    );
 };
--- resources/js/components/dashboard/reactions.tsx
+++ resources/js/components/dashboard/reactions.tsx
@@ -1,45 +1,41 @@
 import { Reaction } from "@/types"
 import { Item, ItemContent, ItemMedia, ItemTitle } from "../ui/item"
-import { useState } from "react"
 import { reactionEmojiLabels } from "@/enums/ReactionEmoji"
+import { useFormatDateAndTime } from "@/hooks/use-format-date-time"
 
 interface ReactionsProps {
     reactions: Reaction[];
 }
 
 export default function Reactions({ reactions }: ReactionsProps) {
-    const [date, setDate] = useState<string>("");
-    const [time, setTime] = useState<string>("");
-
-    function formatDateAndTime(created_at: string) {
-        setDate(created_at.split(',')[0]);
-        setTime(created_at.split(',')[1]);
-
-        return { date, time };
-    }
+    const formatDateAndTime = useFormatDateAndTime;
 
     return (
         <>
             <div className="text-lg pl-6 my-4 font-semibold">Latest Reactions</div>
             <ul className="space-y-4 px-6 mb-6">
-                {reactions.map(reaction => (
-                    <Item
-                        variant={"muted"}
-                        className="items-start"
-                        key={reaction.id}
-                    >
-                        <ItemMedia>
-                            {reactionEmojiLabels[reaction.emoji]}
-                        </ItemMedia>
-                        <ItemContent>
-                            <ItemTitle className="text-xs">{reaction.post?.title}</ItemTitle>
-                        </ItemContent>
-                        <ItemContent>
-                            <p className="text-xs">{formatDateAndTime(new Date(reaction.created_at).toLocaleString()).date}</p>
-                            <p className="text-xs text-muted-foreground">{formatDateAndTime(new Date(reaction.created_at).toLocaleString()).time}</p>
-                        </ItemContent>
-                    </Item>
-                ))}
+                {reactions.map(reaction => {
+                    const { date, time } = formatDateAndTime(new Date(reaction.created_at).toLocaleString());
+
+                    return (
+                        <Item
+                            variant={"muted"}
+                            className="items-start"
+                            key={reaction.id}
+                        >
+                            <ItemMedia>
+                                {reactionEmojiLabels[reaction.emoji]}
+                            </ItemMedia>
+                            <ItemContent>
+                                <ItemTitle className="text-xs">{reaction.post?.title}</ItemTitle>
+                            </ItemContent>
+                            <ItemContent>
+                                <p className="text-xs">{date}</p>
+                                <p className="text-xs text-muted-foreground">{time}</p>
+                            </ItemContent>
+                        </Item>
+                    );
+                })}
             </ul>
         </>
     );
