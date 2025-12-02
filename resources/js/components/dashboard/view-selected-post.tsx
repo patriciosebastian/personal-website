59fb07c4ec6f53bbe5d4808e53b7c53d5b87608a
Patricio Salazar <psebastiansalazar@gmail.com>
2025-12-01 17:12:33 -0800

    added tailwind typography and some style rules

--- resources/js/components/dashboard/view-selected-post.tsx
+++ resources/js/components/dashboard/view-selected-post.tsx
@@ -2,12 +2,12 @@ import { Post } from "@/types"
 
 export default function ViewSelectedPost({ postToPreview }: { postToPreview: Post[] }) {
     return (
-        <div className="p-12">
-            <h3 className="text-xl font-semibold">{postToPreview.title}</h3>
+        <article className="w-fit max-w-lg pt-16 mb-12 lg:mx-auto lg:pt-22 lg:max-w-4xl">
+            <h3 className="text-xl font-semibold lg:text-3xl lg:mb-12">{postToPreview.title}</h3>
             <div
                 dangerouslySetInnerHTML={{ __html: postToPreview.content }}
-                className="prose"
+                className="prose lg:prose-lg dark:prose-invert"
             />
-        </div>
+        </article>
     );
 };
