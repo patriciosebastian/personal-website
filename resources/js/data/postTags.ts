b869cb4a69da6c63d699bc6319c15258c99f936f
Patricio Salazar <psebastiansalazar@gmail.com>
2025-12-02 22:05:08 -0800

    added postTags data

---
+++ resources/js/data/postTags.ts
@@ -0,0 +1,10 @@
+export const postTags = [
+    'is_freelance',
+    'is_web_development',
+    'is_tech',
+    'is_life',
+    'is_entrepreneurship',
+    'is_side_project',
+    'is_product_review',
+    'is_thoughts',
+];
