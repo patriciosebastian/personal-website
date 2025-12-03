1ed523514c7b014f5727869b70c6e70fcc1ccaab
Patricio Salazar <psebastiansalazar@gmail.com>
2025-12-02 22:06:02 -0800

    added some utility helpers

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -1,3 +1,4 @@
+import { Post } from '@/types';
 import { InertiaLinkProps } from '@inertiajs/react';
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
@@ -16,3 +17,23 @@ export function isSameUrl(
 export function resolveUrl(url: NonNullable<InertiaLinkProps['href']>): string {
     return typeof url === 'string' ? url : url.url;
 }
+
+export const getActiveTags = (post: Post, postTags: string[]): string[] => {
+    return postTags
+        .filter((tag) => post[tag as keyof Post])
+        .map((tag) => tag.replace('is_', '').replaceAll('_', ' '));
+};
+
+export const formatDate = (date: string): string => {
+    return new Date(date).toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    });
+};
+
+export const getReadingTime = (content: string): number => {
+    const wordsPerMinute = 200;
+    const wordCount = content.split(/\s+/).length;
+    return Math.ceil(wordCount / wordsPerMinute);
+};
