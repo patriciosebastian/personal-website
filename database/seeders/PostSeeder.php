87fb5519ebb55a0380b54cff3092662a70e207bd
Patricio Salazar <psebastiansalazar@gmail.com>
2025-12-07 08:05:23 -0800

    updated PostSeeder

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Post;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PostSeeder extends Seeder
 {
@@ -12,6 +13,17 @@ class PostSeeder extends Seeder
      */
     public function run(): void
     {
-        Post::factory()->count(5)->create();
+        $maxId = DB::table('posts')->max('id') ?? 0;
+        DB::statement("SELECT setval(pg_get_serial_sequence('posts', 'id'), $maxId)");
+
+        $existingCount = Post::count();
+        $totalNeeded = 54;
+        $toCreate = max(0, $totalNeeded - $existingCount);
+
+        if ($toCreate > 0) {
+            Post::factory()->count($toCreate)->create([
+                'status' => 'published',
+            ]);
+        }
     }
 }
