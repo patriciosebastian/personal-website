f6134192a671a07e665139d245404398baef3eaf
Patricio Salazar <psebastiansalazar@gmail.com>
2026-05-16 08:56:10 -0700

    fix SSR setup for Ziggy routes and add JSX page resolution

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -2,6 +2,7 @@ import { createInertiaApp } from '@inertiajs/react';
 import createServer from '@inertiajs/react/server';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import ReactDOMServer from 'react-dom/server';
+import { type RouteName, route } from 'ziggy-js';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -12,10 +13,26 @@ createServer((page) =>
         title: (title) => (title ? `${title} - ${appName}` : appName),
         resolve: (name) =>
             resolvePageComponent(
-                `./pages/${name}.tsx`,
-                import.meta.glob('./pages/**/*.tsx'),
+                [
+                    `./pages/${name}.tsx`,
+                    `./pages/${name}.jsx`,
+                ],
+                import.meta.glob('./pages/**/*.{tsx,jsx}'),
             ),
         setup: ({ App, props }) => {
+            /* eslint-disable */
+            // @ts-expect-error
+            global.Ziggy = { ...page.props.ziggy, location: new URL(page.props.ziggy.location) };
+            // @ts-expect-error
+            global.route<RouteName> = (name, params, absolute) =>
+                route(name, params as any, absolute, {
+                    // @ts-expect-error
+                    ...page.props.ziggy,
+                    // @ts-expect-error
+                    location: new URL(page.props.ziggy.location),
+                });
+            /* eslint-enable */
+
             return <App {...props} />;
         },
     }),
